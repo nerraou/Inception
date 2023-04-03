8da9aec8a64e7a457011d8dc2de82ead07eea716
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-03 17:30:09 +0000

    wip: create mariadb volume

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+
 mariadbd --user=mysql --bootstrap << EOF
 FLUSH PRIVILEGES;
 
@@ -9,6 +11,9 @@ CREATE OR REPLACE DATABASE ${WORDPRESS_DB_NAME};
 
 CREATE USER '${WORDPRESS_ADMIN}'@'localhost' IDENTIFIED BY '${WORDPRESS_ADMIN_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${WORDPRESS_ADMIN}'@'localhost';
+
+CREATE USER '${WORDPRESS_STANDARD_USER}'@'localhost' IDENTIFIED BY '${WORDPRESS_STANDARD_USER_PASSWORD}';
+
 
 EOF
 
