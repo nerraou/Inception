abc033743b0a571b01a352e759f7563a3f80c3d4
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-24 14:00:07 -0400

    change mariadb root password

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -3,14 +3,18 @@
 mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
 mariadbd --user=mysql --bootstrap << EOF
+
 FLUSH PRIVILEGES;
 
 -- DROP USER ''@'localhost';
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 
 CREATE OR REPLACE DATABASE ${WP_DB_NAME};
 
 CREATE USER '${WP_ADMIN}' IDENTIFIED BY '${WP_ADMIN_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO '${WP_ADMIN}';
+
 
 EOF
 
