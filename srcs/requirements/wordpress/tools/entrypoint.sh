c381f0ab7b06ead9562ee436a248c72ab399252b
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-26 05:13:39 -0400

    clean up

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -6,31 +6,32 @@ while ! mariadb -h$WP_MARIADB_HOSTNAME -u$WP_ADMIN -p$WP_ADMIN_PASSWORD $WP_DB_N
 done
 
 # Install WordPress
-wp core download --allow-root
-
-wp config create --dbname=$WP_DB_NAME \
-					--dbuser=$WP_ADMIN \
-					--dbpass=$WP_ADMIN_PASSWORD \
-					--dbhost=$WP_MARIADB_HOSTNAME \
+if ! wp --allow-root core is-installed; then
+	wp core download --allow-root
+	
+	wp config create --dbname=$WP_DB_NAME \
+						--dbuser=$WP_ADMIN \
+						--dbpass=$WP_ADMIN_PASSWORD \
+						--dbhost=$WP_MARIADB_HOSTNAME \
+						--allow-root
+	
+	wp core install --url=$WP_URL \
+					--title=$WP_TITLE \
+					--admin_user=$WP_ADMIN \
+					--admin_password=$WP_ADMIN_PASSWORD \
+					--admin_email=$WP_ADMIN_EMAIL \
+					--skip-email \
 					--allow-root
-
-wp core install --url=$WP_URL \
-				--title=$WP_TITLE \
-				--admin_user=$WP_ADMIN \
-				--admin_password=$WP_ADMIN_PASSWORD \
-				--admin_email=$WP_ADMIN_EMAIL \
-				--skip-email \
-				--allow-root
-
-wp user create $WP_USER $WP_USER_EMAIL \
-				--role=author \
-				--user_pass=$WP_USER_PASSWORD \
-				--allow-root
-
-wp plugin install redis-cache --activate --allow-root
-
-wp config set WP_REDIS_HOST $WP_REDIS_HOST --allow-root
-
-wp config set WP_REDIS_PORT $WP_REDIS_PORT --allow-root
-
+	
+	wp user create $WP_USER $WP_USER_EMAIL \
+					--role=author \
+					--user_pass=$WP_USER_PASSWORD \
+					--allow-root
+	
+	wp plugin install redis-cache --activate --allow-root
+	
+	wp config set WP_REDIS_HOST $WP_REDIS_HOST --allow-root
+	
+	wp config set WP_REDIS_PORT $WP_REDIS_PORT --allow-root
+fi
 php-fpm7 -F
